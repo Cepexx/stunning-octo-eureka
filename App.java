db9ad4a1eddf94c408521244aed97e7341371f4d
Cepexx <82107568+Cepexx@users.noreply.github.com>
2021-12-02 13:09:50 +0100

    Forzando push

--- App.java
+++ App.java
@@ -2,8 +2,9 @@ public class App {
   
   public static void main(String[] args) {
     System.out.println("Jose Antonio");
-    System.out.println("Contenido 3");
+    System.out.println("Vivo en Cepelandia");
     System.out.println("La fecha de hoy xd");
+    System.out.println("Negro");
   }
   
 }
