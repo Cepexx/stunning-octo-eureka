4fe27b29b93ddf809e29e27479e30d9c86df23df
Cepexx <82107568+Cepexx@users.noreply.github.com>
2021-12-02 13:01:17 +0100

    Modified by Cepex

--- App.java
+++ App.java
@@ -1,9 +1,9 @@
 public class App {
   
   public static void main(String[] args) {
-    System.out.println("Contenido 2a");
+    System.out.println("Jose Antonio");
     System.out.println("Contenido 3");
-    System.out.println("Contenido 2b");
+    System.out.println("La fecha de hoy xd");
   }
   
 }
--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 # stunning-octo-eureka
 
-Aplicación Java sencilla para su uso en el módulo de "Entronos de desarrrollo".
+Aplicación Java sencilla para su uso en el módulo de "Entornos de desarrollo".
 Ilustra el uso de git push entre otros.
+
+
+Cuidao con las faltas
+
+				fdo: Cepe
